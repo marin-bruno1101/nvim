5fd6d0d1ae1f62f3ff551dd540393bbda84c2206
Bruno M. <marin.bruno1101@gmail.com>
2024-01-18 15:26:34 -0300

    feat: add alpha-nvim plugin

---
+++ lua/core/plugins/alpha.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"goolord/alpha-nvim",
+		config = function()
+			local alpha = require("alpha")
+			local dashboard = require("alpha.themes.dashboard")
+
+			dashboard.section.buttons.val = {
+				dashboard.button("n", "  New file", "<cmd>ene<CR>"),
+				dashboard.button("f", "󰈞  Find file", "<cmd>Telescope find_files<CR>"),
+				dashboard.button("e", "󰅚  Explorer", "<cmd>E<CR>"),
+				dashboard.button("r", "  Recents", "<cmd>Telescope oldfiles<CR>"),
+				dashboard.button("p", "󰏔  Sync plugins", "<cmd>Lazy sync<CR>"),
+				dashboard.button("m", "󰏓  Manage plugins", "<cmd>Lazy<CR>"),
+				dashboard.button("s", "  Settings", "<cmd>E $MYVIMRC/..<CR>"),
+				dashboard.button("q", "󰅚  Quit NVIM", "<cmd>qa<CR>"),
+			}
+
+			dashboard.section.footer.val = "github.com/marin-bruno1101"
+
+			alpha.setup(dashboard.config)
+		end,
+	},
+}
+
