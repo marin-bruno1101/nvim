7658e4cb8f69c898642ad1255b72d1599c05cb0e
Bruno M. <marin.bruno1101@gmail.com>
2024-10-10 16:07:18 -0300

    feat: add keymap <leader>fm to format file

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -2,3 +2,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("n", "<leader>fm", function()
+	vim.lsp.buf.format()
+end)
+
